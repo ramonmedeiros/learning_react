8f8d0291423e7a0c53ddd66e0e57b01b085d55e8
Ramon Medeiros <ramon.medeiros@recordunion.com>
2019-11-17 17:31:04 +0100

    Updates

---
+++ precache-manifest.11ba2bc81c42b7e973f8b939a3576449.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98df513e9b85d1887693ebbe2736b305",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "0a27568f28800756b743",
+    "url": "/react-gh-pages/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "445165fb6e8906d98808",
+    "url": "/react-gh-pages/static/js/2.74ae950f.chunk.js"
+  },
+  {
+    "revision": "0a27568f28800756b743",
+    "url": "/react-gh-pages/static/js/main.4f547562.chunk.js"
+  },
+  {
+    "revision": "f90632031ba146fdab11",
+    "url": "/react-gh-pages/static/js/runtime-main.5d6b8f88.js"
+  }
+]);
