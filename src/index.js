0ea900ea6572beb2a09549113dd9449303f9df54
Ramon Medeiros <ramon.medeiros@recordunion.com>
2019-11-18 00:32:52 +0100

    MVP

--- src/index.js
+++ src/index.js
@@ -2,24 +2,66 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => alert('click')}>
-      {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+    {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      history: [],
+      player: 'X',
+    };
+  }
+
+  handleClick(i) {
+    if (calculateWinner(this.generateSquare())) {
+      return;
+    }
+
+
+    this.setState({player: this.nextPlayer(this.state.player),
+                  history: this.state.history.concat({"player": this.state.player,
+                                                      "position": i})
+    });
+    console.log(this.state.history);
+    console.log(this.player);
+  }
+
+  nextPlayer(player){
+    if (player == "X")
+      return "O";
+    return "X";
+  }
+
+  generateSquare(){
+    let squares = Array(9).fill(null);
+
+    for (let move of this.state.history){
+      squares[move["position"]] = move["player"];
+    }
+    return squares;
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square value={this.generateSquare()[i]}
+                   onClick={() => this.handleClick(i)}/>;
   }
 
   render() {
-    const status = 'Next player: X';
-
+    const winner = calculateWinner(this.generateSquare());
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player:' + this.nextPlayer(this.state.player);
+    }
+  
     return (
       <div>
         <div className="status">{status}</div>
@@ -59,6 +101,26 @@ class Game extends React.Component {
   }
 }
 
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 // ========================================
 
 ReactDOM.render(
